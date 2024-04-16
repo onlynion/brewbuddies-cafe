c29295b8b254a0b90bfd3aa3dbeaf21c37978f0a
onlynion <sadikalwahid2018@gamil.com>
2024-04-17 02:27:51 +0600

    first commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,233 @@
+/* eslint-disable react/prop-types */
+import React, { useContext, useState } from "react";
+import { FaHeart } from "react-icons/fa";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthProvider";
+import Swal from "sweetalert2";
+import useCart from "../hooks/useCart";
+import axios from "axios";
+
+const Cards = ({ item }) => {
+  const { name, image, price, recipe, _id } = item;
+
+  const { user } = useContext(AuthContext);
+  const [cart, refetch] = useCart();
+  const navigate = useNavigate();
+  const location = useLocation();
+  // console.log(item)
+  const [isHeartFilled, setIsHeartFilled] = useState(false);
+
+  const handleHeartClick = () => {
+    setIsHeartFilled(!isHeartFilled);
+  };
+
+  // add to cart handler
+  const handleAddToCart = (item) => {
+    // console.log(item);
+    if (user && user.email) {
+      const cartItem = {
+        menuItemId: _id,
+        name,
+        quantity: 1,
+        image,
+        price,
+        email: user.email,
+      };
+
+      axios
+        .post("http://localhost:6001/carts", cartItem)
+        .then((response) => {
+          console.log(response);
+          if (response) {
+            refetch(); // refetch cart
+            Swal.fire({
+              position: "center",
+              icon: "success",
+              title: "Food added on the cart.",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        })
+        .catch((error) => {
+          console.log(error.response.data.message);
+          const errorMessage = error.response.data.message;
+          Swal.fire({
+            position: "center",
+            icon: "warning",
+            title: `${errorMessage}`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        });
+    } else {
+      Swal.fire({
+        title: "Please login to order the food",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Login now!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login", { state: { from: location } });
+        }
+      });
+    }
+  };
+
+  return (
+    <div
+      to={`/menu/${item._id}`}
+      className="card shadow-xl relative mr-5 md:my-5"
+    >
+      <div
+        className={`rating gap-1 absolute right-2 top-2 p-4 heartStar bg-green ${
+          isHeartFilled ? "text-rose-500" : "text-white"
+        }`}
+        onClick={handleHeartClick}
+      >
+        <FaHeart className="w-5 h-5 cursor-pointer" />
+      </div>
+      <Link to={`/menu/${item._id}`}>
+        <figure>
+          <img
+            src={item.image}
+            alt="Shoes"
+            className="hover:scale-105 transition-all duration-300 md:h-72"
+          />
+        </figure>
+      </Link>
+      <div className="card-body">
+        <Link to={`/menu/${item._id}`}>
+          <h2 className="card-title">{item.name}!</h2>
+        </Link>
+        <p>Description of the item</p>
+        <div className="card-actions justify-between items-center mt-2">
+          <h5 className="font-semibold">
+            <span className="text-sm text-red">৳ </span> {item.price}
+          </h5>
+          <button
+            onClick={() => handleAddToCart(item)}
+            className="btn bg-green text-white"
+          >
+            Add to Cart{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
+
+// import { Link, useLocation, useNavigate } from "react-router-dom";
+// import { FaHeart } from "react-icons/fa";
+// import { AuthContext } from "../contexts/AuthProvider";
+// import Swal from "sweetalert2";
+
+// const Cards = ({ item }) => {
+//   const { name, image, price, recipe, _id } = item;
+//   const [isHeartFilled, setIsHeartFilled] = useState(false);
+//   const { user } = useContext(AuthContext);
+//   // console.log(user)
+
+//   const navigate = useNavigate();
+//   const location = useLocation();
+
+//   // add to cart
+//   const handleAddToCart = (item) => {
+//     // console.log("button is clicked", item);
+//     if (user && user?.email) {
+//       const cartItems = {
+//         menuItemId: _id,
+//         name,
+//         quantiy: 1,
+//         image,
+//         price,
+//         email: user.email,
+//       };
+//       // console.log(cartItems);
+//       fetch("http://localhost:6001/carts", {
+//         method: "POST",
+//         headers: {
+//           "content-type": "application/json",
+//         },
+//         body: JSON.stringify(cartItems),
+//       })
+//         .then((res) => res.json())
+//         .then((data) => {
+//           // console.log(data);
+//           if (data.insertedId) {
+//             Swal.fire({
+//               position: "middle",
+//               icon: "success",
+//               title: "Item added to the cart",
+//               showConfirmButton: false,
+//               timer: 1500,
+//             });
+//           } else {
+//             Swal.fire({
+//               title: "Please Login",
+//               text: "Without an account can't able to add products",
+//               icon: "warning",
+//               showCancelButton: true,
+//               confirmButtonColor: "#3085d6",
+//               cancelButtonColor: "#d33",
+//               confirmButtonText: "Signup Now!",
+//             }).then((result) => {
+//               if (result.isConfirmed) {
+//                 navigate('/signup', {state:{from: location}});
+//                 console.log('else working')
+//               }
+//             });
+//           }
+//         });
+//     }
+//   };
+
+//   const handleHeartClick = () => {
+//     setIsHeartFilled(!isHeartFilled);
+//   };
+//   return (
+//     <div className="card shadow-xl bg-base-200 relative mr-5 md:my-5">
+//       <div
+//         className={`rating gap-1 absolute right-2 top-2 p-4 heartStar bg-green ${
+//           isHeartFilled ? "text-rose-500" : "text-white"
+//         }`}
+//         onClick={handleHeartClick}
+//       >
+//         <FaHeart className="h-5 w-5 cursor-pointer" />
+//       </div>
+//       <Link to={`/menu/${item._id}`}>
+//         <figure>
+//           <img
+//             src={item.image}
+//             alt="Shoes"
+//             className="hover:scale-105 transition-all duration-200 md:h-72"
+//           />
+//         </figure>
+//       </Link>
+//       <div className="card-body">
+//         <Link to={`/menu/${item._id}`}>
+//           <h2 className="card-title">{item.name}</h2>
+//         </Link>
+//         <p>Description of the item</p>
+//         <div className="card-actions justify-between items-center my-2">
+//           <h5 className="font-semibold">
+//             <span className="text-sm text-red">৳</span>
+//             {item.price}
+//           </h5>
+//           <button
+//             className="btn bg-green text-white"
+//             onClick={() => handleAddToCart(item)}
+//           >
+//             Add to cart
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Cards;
---
+++ src/pages/menuPage/CheckoutForm.jsx
@@ -0,0 +1,160 @@
+/* eslint-disable react/prop-types */
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import React, { useEffect, useState } from "react";
+import { FaPaypal } from "react-icons/fa";
+import useAuth from "../../hooks/useAuth";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { useNavigate } from "react-router-dom";
+
+const CheckoutForm = ({ price, cart }) => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const{user} = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const navigate = useNavigate();
+
+    const [cardError, setCardError] = useState('');
+    const [clientSecret, setClientSecret] = useState("");
+
+    useEffect(()=>{
+      if(typeof price !== 'number' || price < 1){
+        console.log("Price is not an number or less than 1")
+        return;
+      }
+      axiosSecure.post('/create-payment-intent', {price})
+      .then(res => {
+        console.log(res.data.clientSecret)
+        setClientSecret(res.data.clientSecret)
+      })
+    },[price,axiosSecure])
+    
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!stripe || !elements) {
+        // Stripe.js has not loaded yet. Make sure to disable
+        // form submission until Stripe.js has loaded.
+        return;
+      }  
+    
+    //   Create a card elemenet
+    const card = elements.getElement(CardElement);
+
+    if (card == null) {
+      return;
+    }
+
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+        type: 'card',
+        card,
+      });
+  
+      if (error) {
+        console.log('[error]', error);
+        setCardError(error.message);
+      } else {
+        setCardError("Success!")
+        console.log('[PaymentMethod]', paymentMethod);
+      }
+
+      const {paymentIntent, error: confirmError} = await stripe.confirmCardPayment(
+        clientSecret,
+        {
+          payment_method: {
+            card: card,
+            billing_details: {
+              name: user?.displayName || 'anonymous',
+              email: user?.email || 'unknown'
+            },
+          },
+        },
+      );
+
+      if(confirmError){
+        console.log(confirmError)
+      }
+      console.log(paymentIntent)
+      if(paymentIntent.status === "succeeded"){
+        console.log(paymentIntent.id)
+        setCardError(`Your transaction id is ${paymentIntent.id}`)
+        // payment info data 
+        const paymentInfo = {
+          email: user.email,
+          transactionId: paymentIntent.id,
+          price,
+          quantity: cart.length,
+          status: "Order Pending",
+          itemName: cart.map(item => item.name),
+          cartItems: cart.map(item => item._id),
+          menuItems:  cart.map(item => item.menuItemId)
+        }
+        console.log(paymentInfo);
+        // send information to backend
+        axiosSecure.post('/payments', paymentInfo)
+        .then ( res => {
+          console.log(res.data);
+          alert("Payment successfull");
+          navigate('/order')
+        })
+      }
+  };
+  return (
+    <div className="flex flex-col sm:flex-row justify-start items-start gap-8">
+      {/* left side */}
+      <div className="md:w-1/2 w-full space-y-3">
+        <h4 className="text-lg font-semibold">Order Summary</h4>
+        <p>Total Price: ৳{price}</p>
+        <p>Number of Items: {cart.length}</p>
+      </div>
+
+      {/* right side */}
+      <div className="md:w-1/3 space-y-5  card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 px-4 py-8">
+        <h4 className="text-lg font-semibold">Process Your Payment</h4>
+        <h5 className="font-medium">Credit/Debit Card</h5>
+        {/* Stripe form */}
+        <form onSubmit={handleSubmit}>
+          <CardElement
+            options={{
+              style: {
+                base: {
+                  fontSize: "16px",
+                  color: "#424770",
+                  "::placeholder": {
+                    color: "#aab7c4",
+                  },
+                },
+                invalid: {
+                  color: "#9e2146",
+                },
+              },
+            }}
+          />
+          <button
+            type="submit"
+            disabled={!stripe}
+            className="btn btn-sm mt-5 bg-orange-500 text-white btn-primary w-full border-none"
+          >
+            Pay
+          </button>
+        </form>
+
+        {
+            cardError ? <p className="text-red italic text-xs">{cardError}</p> : ""
+        }
+
+        {/* paypal */}
+        <div className="mt-5 text-center">
+            <hr />
+            <button
+            type="submit"
+            className="btn btn-sm mt-5 bg-orange-500 text-white"
+          >
+            <FaPaypal/> Pay with paypal
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutForm;
